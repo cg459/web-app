aebc44029829f4e48f8d6a447fabbd39612eb2f4
cg459 <47122055+cg459@users.noreply.github.com>
2019-12-14 23:34:34 -0500

    Add files via upload

---
+++ completed.php
@@ -0,0 +1,113 @@
+<?php
+    require "../view/header.php";
+    require('../model/PDObject.php');
+    require('../model/todos_task.php');
+    require('../model/buttonFunctions.php');
+    global $db;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<script src="../functions/deleteFunc.js"></script>
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="../css/main2.css" rel="stylesheet">
+</head>
+<body>
+<main>
+    <h1 class="title1">Tasks that are completed</h1>
+    <div class="links">
+        <a href="../main/index.php"class="tasks">Tasks</a>
+        <a href="completed.php" class="tasks2">CompletedTasks</a>
+    </div>
+
+<?php
+    $ga = new bt();
+            if(isset($_POST['delete'])){
+                $id = $_POST['id'];
+                $sth = $db->prepare("delete from todos where id = :id");
+                $sth->bindValue(':id', $id, PDO::PARAM_INT);
+                $sth->execute();
+            }
+            
+            if(isset($_POST['complete'])){
+                $id1 = $_POST['id'];
+                $sth1 = $db->prepare("UPDATE todos SET isdone = 0 WHERE id = :id");
+                $sth1->bindValue(':id', $id1, PDO::PARAM_INT);
+                $sth1->execute();
+            }
+    
+            if(isset($_POST['edit'])){
+                $id1 = $_POST['id'];
+                header("Location: ../edit/edit1.php");
+                
+                
+            }
+            $gt = new TasksFunc();
+            $completed_tasks = $gt->get_iscompleted($_SESSION['uemail']);
+            
+?>
+
+        <div class='table'>
+            <form method="POST">
+                <table class='center'border=\1\>
+                    <tr class='trhead'>
+                        <th style='padding:40px'>Date Created</th>
+                        <th style='padding:40px'>Message</th>
+                        <th style='padding:40px'>Deadline</th>
+                        <th style='padding:40px'>Title</th>
+                        <th style='padding:40px'>TaskID</th>
+                        <th style='padding:40px'>Buttons</th>
+                        <style>table.center{margin-left:auto;margin-right:auto;}</style>
+                    </tr>
+<?php
+                for ($i = 0; $i<count($completed_tasks); $i++)
+                {
+?>
+
+            <tr>
+                <td style='padding:40px'> <?=  $completed_tasks[$i]["createddate"] ?></td>
+                <td style='padding:40px' name=check_list[1]> <?= $completed_tasks[$i]["message"] ?> </td>
+                <td style='padding:40px'> <?= $completed_tasks[$i]["duedate"] ?> </td>
+                <td style='padding:40px'> <?= $completed_tasks[$i]["Title"] ?> </td>
+                <td style='padding:40px'> <?= $completed_tasks[$i]["id"] ?> </td>
+                <td style='padding:40px'>
+                
+                <form>
+                    <div class="forDelete">
+                        <button type="submit" name="delete">Delete</button>
+                        <input type="hidden" name="id" value="<?= $completed_tasks[$i]['id'] ?>">
+                        <input type="hidden" name="delete" value="true">
+                    </div>
+                </form>
+            <form method="POST">
+                <div class="forComplete">
+                    <button type="submit" name="complete">uncomplete</button>
+                    <input type="hidden" name="id" value="<?= $completed_tasks[$i]['id'] ?>">
+                    <input type="hidden" name="complete" value="true">
+                </div>
+            </form>
+            <form method="POST" action='../edit/edit1.php'>
+                <div class="foredit">
+                    <a href="../edit/edit.php"><input type="submit" name="edit" value="edit" href="../edit/edit.php"></input></a>
+                    <input type="hidden" name="id1" value="<?= $completed_tasks[$i]['id'] ?>">
+                    <input type="hidden" name="edit" value="true">
+                </div>
+            </form>
+</form>
+                </td>
+    <?php
+            
+        }
+?>
+        </table>
+        <div class='button1'>
+                <a href='../new/new.php'id='new'  class='new'>New</a>
+        </div>
+            <style> button{margin-right:10%; margin-left: 10%; margin-top: 5%; display:inline;}</style>
+        </form>
+    </main>
+</body>
+
+
